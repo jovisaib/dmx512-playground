6b0f57972f9d4d0128eb8f93dbe63d5039de3a03
jvsaez <jv.saez@nayarsystems.com>
2020-11-27 14:11:08 +0100

    wip

--- main.go
+++ main.go
@@ -18,22 +18,22 @@ func main() {
 	for {
 		time.Sleep(1000 * time.Millisecond)
 
-		dmx.SetChannel(26, byte(r.Intn(256)))
+		dmx.SetChannel(26, 200)
 		dmx.SetChannel(27, byte(r.Intn(256)))
 		dmx.SetChannel(28, byte(r.Intn(256)))
 		dmx.SetChannel(29, byte(r.Intn(256)))
 
-		dmx.SetChannel(31, byte(r.Intn(256)))
+		dmx.SetChannel(31, 200)
 		dmx.SetChannel(32, byte(r.Intn(256)))
 		dmx.SetChannel(33, byte(r.Intn(256)))
 		dmx.SetChannel(34, byte(r.Intn(256)))
 
-		dmx.SetChannel(36, byte(r.Intn(256)))
+		dmx.SetChannel(36, 200)
 		dmx.SetChannel(37, byte(r.Intn(256)))
 		dmx.SetChannel(38, byte(r.Intn(256)))
 		dmx.SetChannel(39, byte(r.Intn(256)))
 
-		dmx.SetChannel(41, byte(r.Intn(256)))
+		dmx.SetChannel(41, 200)
 		dmx.SetChannel(42, byte(r.Intn(256)))
 		dmx.SetChannel(43, byte(r.Intn(256)))
 		dmx.SetChannel(44, byte(r.Intn(256)))
