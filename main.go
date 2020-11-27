2f1abacad90d6404f26cf6c79c4fd8eefb711627
jvsaez <jv.saez@nayarsystems.com>
2020-11-27 13:47:53 +0100

    wip

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/akualab/dmx"
 )
@@ -10,52 +11,54 @@ func main() {
 
 	dmx, e := dmx.NewDMXConnection("/dev/ttyUSB0")
 	if e != nil {
-		log.Fatal(e)
+		log.Fatal("ERROR: ", e)
 	}
 
-
 	for {
+
+		time.Sleep(500 * time.Millisecond)
+
 		// Set values for channels.
 		dmx.SetChannel(1, 100)
 		dmx.SetChannel(2, 70)
 		dmx.SetChannel(3, 130)
 		dmx.SetChannel(4, 180)
-		
+
 		dmx.SetChannel(5, 100)
 		dmx.SetChannel(6, 70)
 		dmx.SetChannel(7, 130)
 		dmx.SetChannel(8, 180)
-	
+
 		dmx.SetChannel(9, 100)
 		dmx.SetChannel(10, 70)
 		dmx.SetChannel(11, 130)
 		dmx.SetChannel(12, 180)
-	
+
 		dmx.SetChannel(13, 100)
 		dmx.SetChannel(14, 70)
 		dmx.SetChannel(15, 130)
 		dmx.SetChannel(16, 180)
-	
+
 		dmx.SetChannel(26, 100)
 		dmx.SetChannel(27, 70)
 		dmx.SetChannel(28, 130)
 		dmx.SetChannel(29, 180)
-	
-		dmx.SetChannel(30, 100)
-		dmx.SetChannel(31, 70)
-		dmx.SetChannel(32, 130)
-		dmx.SetChannel(33, 180)
-	
-		dmx.SetChannel(34, 100)
-		dmx.SetChannel(35, 70)
-		dmx.SetChannel(36, 130)
-		dmx.SetChannel(37, 180)
-	
-		dmx.SetChannel(38, 100)
-		dmx.SetChannel(39, 70)
-		dmx.SetChannel(40, 130)
-		dmx.SetChannel(41, 180)
-	
+
+		dmx.SetChannel(31, 100)
+		dmx.SetChannel(32, 70)
+		dmx.SetChannel(33, 130)
+		dmx.SetChannel(34, 180)
+
+		dmx.SetChannel(36, 100)
+		dmx.SetChannel(37, 70)
+		dmx.SetChannel(38, 130)
+		dmx.SetChannel(39, 180)
+
+		dmx.SetChannel(41, 100)
+		dmx.SetChannel(42, 70)
+		dmx.SetChannel(43, 130)
+		dmx.SetChannel(44, 180)
+
 		// Send!
 		dmx.Render()
 	}
