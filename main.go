bd16cab569802dcffa490e137305f129af22769c
jvsaez <jv.saez@nayarsystems.com>
2020-11-27 18:02:27 +0100

    wip

--- main.go
+++ main.go
@@ -1,66 +1,44 @@
 package main
 
 import (
+	"github.com/akualab/dmx"
 	"log"
+	"math/rand"
 	"time"
-
-	"github.com/jovisaib/dmx512-playground/dmx"
 )
 
 func main() {
-	// r := rand.New(rand.NewSource(99))
+
+	r := rand.New(rand.NewSource(99))
+	log.Printf("start DMX")
 	dmx, e := dmx.NewDMXConnection("/dev/ttyUSB0")
 	if e != nil {
-		log.Fatal("ERROR: ", e)
+		log.Fatal(e)
 	}
 
-	defer dmx.Close()
+	// Send RGB
+	// 1: brightness/flash, 2: red, 3: blue, 4: green
+	dmx.ChannelMap(34, 31, 33, 32)
+	dmx.SendRGB(130, 100, 100, 100)
+	time.Sleep(10 * time.Second)
+
+	// Initial color.
+	dmx.SetChannel(34, 100)
+	dmx.SetChannel(32, 70)
+	dmx.SetChannel(33, 130)
+	dmx.SetChannel(31, 255)
+	dmx.Render()
 
 	for {
-		time.Sleep(70 * time.Millisecond)
 
-		dmx.SetChannel(26, 255)
-		dmx.SetChannel(27, 255)
-		dmx.SetChannel(28, 255)
-		dmx.SetChannel(29, 255)
+		// Wait.
+		time.Sleep(90 * time.Millisecond)
 
-		if err := dmx.Render(); err != nil {
-			log.Fatal("RENDER ERROR: ", err)
-		}
+		dmx.SetChannel(34, 255)               // Intensity
+		dmx.SetChannel(32, byte(r.Intn(256))) // R
+		dmx.SetChannel(33, byte(r.Intn(256))) // G
+		dmx.SetChannel(31, byte(r.Intn(256))) // B
+		dmx.Render()
 
-		log.Println("RENDER 1!")
-
-		dmx.SetChannel(31, 255)
-		dmx.SetChannel(32, 255)
-		dmx.SetChannel(33, 255)
-		dmx.SetChannel(34, 255)
-
-		if err := dmx.Render(); err != nil {
-			log.Fatal("RENDER ERROR: ", err)
-		}
-
-		log.Println("RENDER 2!")
-
-		dmx.SetChannel(36, 255)
-		dmx.SetChannel(37, 255)
-		dmx.SetChannel(38, 255)
-		dmx.SetChannel(39, 255)
-
-		if err := dmx.Render(); err != nil {
-			log.Fatal("RENDER ERROR: ", err)
-		}
-
-		log.Println("RENDER 3!")
-
-		dmx.SetChannel(41, 255)
-		dmx.SetChannel(42, 255)
-		dmx.SetChannel(43, 255)
-		dmx.SetChannel(44, 255)
-
-		if err := dmx.Render(); err != nil {
-			log.Fatal("RENDER ERROR: ", err)
-		}
-
-		log.Println("RENDER 4!")
 	}
 }
